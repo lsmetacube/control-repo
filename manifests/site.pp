27b3ec09454a2e603807e42a6d11f1699d5a48fe
lsmetacube <lokesh.sharma@metacube.com>
2020-06-27 14:45:21 +0530

    example push

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,31 +1,35 @@
 ## site.pp ##
 
-# This file (./manifests/site.pp) is the main entry point
+# This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
 # used when an agent connects to a master and asks for an updated configuration.
-# https://puppet.com/docs/puppet/latest/dirs_manifest.html
 #
 # Global objects like filebuckets and resource defaults should go in this file,
-# as should the default node definition if you want to use it.
+# as should the default node definition. (The default node can be omitted
+# if you use the console and don't define any other nodes in site.pp. See
+# https://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
+# node definitions.)
 
 ## Active Configurations ##
 
 # Disable filebucket by default for all File resources:
-# https://github.com/puppetlabs/docs-archive/blob/master/pe/2015.3/release_notes.markdown#filebucket-resource-no-longer-created-by-default
+#https://docs.puppet.com/pe/2015.3/release_notes.html#filebucket-resource-no-longer-created-by-default
 File { backup => false }
 
-## Node Definitions ##
+# DEFAULT NODE
+# Node definitions in this file are merged with node data from the console. See
+# https://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
+# node definitions.
 
 # The default node definition matches any node lacking a more specific node
-# definition. If there are no other node definitions in this file, classes
-# and resources declared in the default node definition will be included in
-# every node's catalog.
-#
-# Note that node definitions in this file are merged with node data from the
-# Puppet Enterprise console and External Node Classifiers (ENC's).
-#
-# For more on node definitions, see: https://puppet.com/docs/puppet/latest/lang_node_definitions.html
+# definition. If there are no other nodes in this file, classes declared here
+# will be included in every node's catalog, *in addition* to any classes
+# specified in the console for that node.
+
 node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
+  # this allows us to auto include a role called
+  # role::$pp_role tag
+  # NOTE: This is cause a catalog to fail to compile if role::pp_role doesn't exist
+  if $trusted['extensions']['pp_role'] != undef {
+    include "role::${trusted['extensions']['pp_role']}"
+  }
 }

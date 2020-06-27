f8a18ed8b8372f9d21a7ce01f0f9bf5c5db04f5e
lsmetacube <lokesh.sharma@metacube.com>
2020-06-27 20:14:42 +0530

    fixed pp issue.

--- site/postgresql/server/manifests/db.pp
+++
@@ -1,7 +0,0 @@
-class { 'postgresql::server':
-}
-
-postgresql::server::db { 'mydatabasename':
-  user     => 'mydatabaseuser',
-  password => postgresql::postgresql_password('mydatabaseuser', 'mypassword'),
-}
